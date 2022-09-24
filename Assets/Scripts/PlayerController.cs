3663334dab65c2b16d2ee4d0427b360b7a85890d
vaishali161197vs <vaishali161197@outlook.com>
2022-09-24 21:38:32 +0530

    Scripting Animation and Flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float playerSpeed = 10f;
+    [SerializeField] Animator animator;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    void Run()
+    {
+        float speed = Input.GetAxis("Horizontal");
+        spriteRenderer.flipX = speed > 0 ? false : true;
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        rb.velocity = new Vector2(playerSpeed * speed * Time.deltaTime, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision: " + collision.gameObject.name);
     }
 }
