fabf679b379b54d3dbb7f00555119042a28afc0e
vaishali161197vs <vaishali161197@outlook.com>
2022-11-05 21:59:05 +0530

    Player death by falling

---
+++ Assets/Scripts/BottomCollider.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player Dead by Falling Off the Platform.");
+        }
+    }
+}
