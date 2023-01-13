8be0a6ead72c136d285cffb81f22c158fbf85b7f
vaishali161197vs <vaishali161197vs@outlook.com>
2023-01-13 22:40:32 +0530

    Level Selection Setup

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 public class ScoreController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
     int score = 0;
+    int currentLevel = 0;
     private void Awake()
     {
         scoreText = GetComponent<TextMeshProUGUI>();
